863975600ef3cf6acc2c95e33b33244ac4292bbc
Pedro Miguel Lopes Chaleira <plopes-c@c1r20s10.42lisboa.com>
2022-10-25 23:36:42 +0100

    add isprint && rm strncpy

--- ft_isprint.c
+++ ft_isprint.c
@@ -1,35 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strncpy.c                                       :+:      :+:    :+:   */
+/*   ft_isprint.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: plopes-c <plopes-c@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/25 20:26:47 by plopes-c          #+#    #+#             */
-/*   Updated: 2022/10/25 20:28:08 by plopes-c         ###   ########.fr       */
+/*   Created: 2022/10/25 23:11:56 by plopes-c          #+#    #+#             */
+/*   Updated: 2022/10/25 23:19:23 by plopes-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
-char	*ft_strcpy(char *src, char *dest, int n)
-{
-	int	i;
+#include <unistd.h>
 
-	i = 0;
-	while (src[i] != '\0' && i < n)
-	{
-		dest[i] = src[i];
-		i++;
-	}
-	dest[i] = '\0';
-	return (dest);
+void	ft_isprint(char a)
+{
+	if (a >= 32 && a <= 127)
+		write(1, "1\n", 2);
+	else if (a >= 97 && a <= 122)
+		write(1, "1\n", 2);
+	else
+		write(1, "0\n", 2);
 }
 /*
-int main()
+int	main(void)
 {
-    char src[] = "chupamos cabron pedejo hij de um pinche";
-    char dest[1];
-    ft_strcpy(src, dest, 5);
-    printf("%s\n", dest);
+	ft_isprint(128);
+	return (0);
 }
 */
